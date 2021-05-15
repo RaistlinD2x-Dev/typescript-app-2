eccb5a39264ef6b6aa4bd53289aa23368c0fac74
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-15 12:56:28 -0500

    abstract classes and refactoring

--- build/index.js
+++ build/index.js
@@ -1,8 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
-var NumbersCollection_1 = require("./NumbersCollection");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0, 21, 33, 55, -56, 92183091283, -9324984, 1999, 893742, 8273892]);
-var sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
+var LinkedList_1 = require("./LinkedList");
+// const numbersCollection = new NumbersCollection([10, 3, -5, 324984, 1999, 893742, 8273892])
+// numbersCollection.sort();
+// console.log(numbersCollection.data);
+// // const sorter = new Sorter(numbersCollection);
+// // sorter.sort();
+// // console.log(numbersCollection.data)
+// const charactersCollection = new CharactersCollection('XaAAAayBLonMzZknikASD')
+// charactersCollection.sort();
+// console.log(charactersCollection.data)
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(4);
+linkedList.add(-3);
+linkedList.sort();
+linkedList.print();
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print()
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0, 21, 33, 55, -56, 92183091283, -9324984, 1999, 893742, 8273892])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data)
+
+// const numbersCollection = new NumbersCollection([10, 3, -5, 324984, 1999, 893742, 8273892])
+// numbersCollection.sort();
+// console.log(numbersCollection.data);
+// // const sorter = new Sorter(numbersCollection);
+// // sorter.sort();
+// // console.log(numbersCollection.data)
+
+// const charactersCollection = new CharactersCollection('XaAAAayBLonMzZknikASD')
+// charactersCollection.sort();
+// console.log(charactersCollection.data)
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(4);
+linkedList.add(-3);
+linkedList.sort();
+linkedList.print()
+
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print()
